4bc7f12e97d7f48aa85f7ccda13dd4a70060616f
Иванов Сергей <ivanov.sergey@ocrv.ru>
2021-04-13 11:33:09 +0800

    Add btn charts

--- js/chart.js
+++ js/chart.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.chips');
-    let instances = M.Chips.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.modal');
-    let instances = M.Modal.init(elems, options);
-  });
+  let elems2 = document.querySelectorAll('.chips');
+  let instances2 = M.Chips.init(elems2);
+
+  let elems3 = document.querySelectorAll('.modal');
+  let instances3 = M.Modal.init(elems3);
+});
 
 const labels = [
     '1',
@@ -14,7 +15,6 @@ const labels = [
     '3',
     '4',
 ];
-
 const data = {
     labels: labels,
     datasets: [
@@ -25,7 +25,7 @@ const data = {
         borderColor: '#727cf5',
         borderWidth: 2,
         hoverBorderWidth: 6,
-        data: [456.93, 708.16, 1048.44, ],
+        data: [456.93, 456.93, 1048.44, ],
       },
       {
         label: 'Окт прогноз',
@@ -92,6 +92,7 @@ const data = {
                   drawOnChartArea: true,
                   drawTicks: true,
                 },
+                // stacked: true,
                 ticks: {
                   fontColor: "#344050",
                   callback: function(val, index) {
@@ -103,22 +104,93 @@ const data = {
                 gridLines : {
                     display : false
                 },
+                // stacked: true,
                 ticks: {
                   fontColor: "#344050"
                 }
             }]
         }
       },
-  };
+};
+
+const configBar = {
+  type: 'bar',
+  data: data,
+  legend: {
+    labels: {
+      fontColor: '#fff'
+   }
+},
+  options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'bottom',
+        labels: {
+          fontColor: '#344050'
+        }
+      },
+      scales:{
+          xAxes: [{
+              gridLines: {
+                display: true,
+                drawBorder: true,
+                drawOnChartArea: true,
+                drawTicks: true,
+              },
+              // stacked: true,
+              ticks: {
+                fontColor: "#344050",
+                callback: function(val, index) {
+                  return val + ' квартал';
+                },
+              }
+          }],
+          yAxes: [{
+              gridLines : {
+                  display : false
+              },
+              // stacked: true,
+              ticks: {
+                fontColor: "#344050"
+              }
+          }]
+      }
+    },
+};
 
 var salesChart = new Chart(
     document.getElementById('salesChart'),
     config
 );
+
+var salesChart2 = new Chart(
+  document.getElementById('salesChart2'),
+  config
+);
+
+var salesChart3 = new Chart(
+  document.getElementById('salesChart3'),
+  config
+);
+
+document.getElementById("iconChartLine").style.display = 'none';
+// document.getElementById("iconDisplayList").style.display = 'none';
+
 salesChart.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
 salesChart.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
 salesChart.options.legend.labels.fontColor = '#5e6e82';
 salesChart.update();
+
+salesChart2.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
+salesChart2.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
+salesChart2.options.legend.labels.fontColor = '#5e6e82';
+salesChart2.update();
+
+salesChart3.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
+salesChart3.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
+salesChart3.options.legend.labels.fontColor = '#5e6e82';
+salesChart3.update();
 
 checkbox.addEventListener('change', function() {
     if (this.checked) {
@@ -126,11 +198,31 @@ checkbox.addEventListener('change', function() {
       salesChart.options.scales.xAxes[0].ticks.fontColor = '#fff';
       salesChart.options.legend.labels.fontColor = '#fff';
       salesChart.update();
+
+      salesChart2.options.scales.yAxes[0].ticks.fontColor = '#fff';
+      salesChart2.options.scales.xAxes[0].ticks.fontColor = '#fff';
+      salesChart2.options.legend.labels.fontColor = '#fff';
+      salesChart2.update();
+
+      salesChart3.options.scales.yAxes[0].ticks.fontColor = '#fff';
+      salesChart3.options.scales.xAxes[0].ticks.fontColor = '#fff';
+      salesChart3.options.legend.labels.fontColor = '#fff';
+      salesChart3.update();
     } else {
       salesChart.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
       salesChart.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
       salesChart.options.legend.labels.fontColor = '#5e6e82';
       salesChart.update();
+
+      salesChart2.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
+      salesChart2.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
+      salesChart2.options.legend.labels.fontColor = '#5e6e82';
+      salesChart2.update();
+
+      salesChart3.options.scales.yAxes[0].ticks.fontColor = '#5e6e82';
+      salesChart3.options.scales.xAxes[0].ticks.fontColor = '#5e6e82';
+      salesChart3.options.legend.labels.fontColor = '#5e6e82';
+      salesChart3.update();
     }
 });
 
@@ -140,6 +232,16 @@ chip1.addEventListener('click', function() {
         return (obj.label != 'Окт' && obj.label != 'Окт прогноз'); 
     });
     salesChart.update();
+
+    salesChart2.data.datasets = salesChart2.data.datasets.filter(function(obj) {
+      return (obj.label != 'Окт' && obj.label != 'Окт прогноз'); 
+    });
+    salesChart2.update();
+
+    salesChart3.data.datasets = salesChart3.data.datasets.filter(function(obj) {
+      return (obj.label != 'Окт' && obj.label != 'Окт прогноз'); 
+    });
+    salesChart3.update();
 });
 
 var chip2 = document.getElementById("chip2");
@@ -148,6 +250,16 @@ chip2.addEventListener('click', function() {
         return (obj.label != 'Клнг'); 
     });
     salesChart.update();
+
+    salesChart2.data.datasets = salesChart2.data.datasets.filter(function(obj) {
+      return (obj.label != 'Клнг'); 
+    });
+    salesChart2.update();
+
+    salesChart3.data.datasets = salesChart3.data.datasets.filter(function(obj) {
+      return (obj.label != 'Клнг'); 
+    });
+    salesChart3.update();
 });
 
 var chip3 = document.getElementById("chip3");
@@ -156,6 +268,16 @@ chip3.addEventListener('click', function() {
         return (obj.label != 'Моск'); 
     });
     salesChart.update();
+
+    salesChart2.data.datasets = salesChart2.data.datasets.filter(function(obj) {
+      return (obj.label != 'Моск'); 
+    });
+    salesChart2.update();
+
+    salesChart3.data.datasets = salesChart3.data.datasets.filter(function(obj) {
+      return (obj.label != 'Моск'); 
+    });
+    salesChart3.update();
 });
 
 var chip4 = document.getElementById("chip4");
@@ -164,4 +286,104 @@ chip4.addEventListener('click', function() {
         return (obj.label != 'Горьк'); 
     });
     salesChart.update();
+
+    salesChart2.data.datasets = salesChart2.data.datasets.filter(function(obj) {
+      return (obj.label != 'Горьк'); 
+    });
+    salesChart2.update();
+
+    salesChart3.data.datasets = salesChart3.data.datasets.filter(function(obj) {
+      return (obj.label != 'Горьк'); 
+    });
+    salesChart3.update();
 });
+
+
+function changeViewChart(chartView) {
+  salesChart.destroy();
+  salesChart = new Chart(
+    document.getElementById('salesChart'),
+    chartView === 'line' ? config : configBar
+  );
+
+  salesChart2.destroy();
+  salesChart2 = new Chart(
+    document.getElementById('salesChart2'),
+    chartView === 'line' ? config : configBar
+  );
+
+  salesChart3.destroy();
+  salesChart3 = new Chart(
+    document.getElementById('salesChart3'),
+    chartView === 'line' ? config : configBar
+  );
+
+  if(chartView === 'line') {
+    document.getElementById("iconChartLine").style.display = 'none';
+    document.getElementById("iconChartBar").style.display = 'block';
+  } else {
+    document.getElementById("iconChartBar").style.display = 'none';
+    document.getElementById("iconChartLine").style.display = 'block';
+  }
+}
+
+
+var chartWrapper = document.getElementById("chartWrapper");
+var chartWrapper2 = document.getElementById("chartWrapper2");
+var chartWrapper3 = document.getElementById("chartWrapper3");
+
+function changeDisplayChart(displayViews) {
+  if(displayViews === 'row') {
+    chartWrapper.classList.remove('s4');
+    chartWrapper.classList.remove('m6');
+    chartWrapper.classList.add('s12');
+    chartWrapper2.classList.remove('s4');
+    chartWrapper2.classList.remove('m6');
+    chartWrapper2.classList.add('s12');
+    chartWrapper3.classList.remove('s4');
+    chartWrapper3.classList.remove('m6');
+    chartWrapper3.classList.add('s12');
+    chartWrapper.style.height = 'calc(91vh - 60px - 20px - 20px)';
+    chartWrapper2.style.height = 'calc(91vh - 60px - 20px - 20px)';
+    chartWrapper3.style.height = 'calc(91vh - 60px - 20px - 20px)';
+    // #chartWrapper, #chartWrapper2, #chartWrapper3 {
+    //   position: relative;
+    //   height: calc(91vh - 60px - 20px - 20px);
+    // }
+
+    // document.getElementById("iconDisplayList").style.display = 'none';
+    // document.getElementById("iconDisplayRow").style.display = 'block';
+  } else if(displayViews === 'table') {
+    chartWrapper.classList.remove('s4');
+    chartWrapper.classList.add('s12');
+    chartWrapper.classList.add('m6');
+    chartWrapper2.classList.remove('s4');
+    chartWrapper2.classList.add('s12');
+    chartWrapper2.classList.add('m6');
+    chartWrapper3.classList.remove('s4');
+    chartWrapper3.classList.add('s12');
+    chartWrapper3.classList.add('m6');
+    chartWrapper.style.height = 'calc(50vh - 60px - 20px - 20px)';
+    chartWrapper2.style.height = 'calc(50vh - 60px - 20px - 20px)';
+    chartWrapper3.style.height = 'calc(50vh - 60px - 20px - 20px)';
+
+    // document.getElementById("iconDisplayList").style.display = 'none';
+    // document.getElementById("iconDisplayRow").style.display = 'block';
+  }else {
+    chartWrapper.classList.remove('s12');
+    chartWrapper.classList.remove('m6');
+    chartWrapper.classList.add('s4');
+    chartWrapper2.classList.remove('s12');
+    chartWrapper2.classList.remove('m6');
+    chartWrapper2.classList.add('s4');
+    chartWrapper3.classList.remove('s12');
+    chartWrapper3.classList.remove('m6');
+    chartWrapper3.classList.add('s4');
+    chartWrapper.style.height = 'calc(91vh - 60px - 20px - 20px)';
+    chartWrapper2.style.height = 'calc(91vh - 60px - 20px - 20px)';
+    chartWrapper3.style.height = 'calc(91vh - 60px - 20px - 20px)';
+
+    // document.getElementById("iconDisplayRow").style.display = 'none';
+    // document.getElementById("iconDisplayList").style.display = 'block';
+  }
+}
